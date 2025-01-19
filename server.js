df4ae7dec051c2b0f8cbc343d62c384f820aeb41
vikas <bhosalevikas2006@gmail.com>
2025-01-20 02:49:28 +0530

    fist commit

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost:27017/')
+console.log('Connected to MongoDB');
+const productSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  price: Number,
+  stock: Number
+});
+
+const product = mongoose.model('items', productSchema);
+
+app.use(express.json());
+
+app.get('/get', async (req, res) => {
+ 
+  res.send('hello');
+});
+
+app.get('/get/:id', async (req, res) => {
+  const id = req.params.id;
+  
+  const data = await product.findById(id);
+  res.json(data);
+});
+
+app.post("/post", async (req, res) => {
+  try {
+    const { name, category, price, stock } = req.body;
+    const productExist = await product.findOne({ name });
+ 
+    if (productExist) {
+      return res.json({ message: "product already exists" });
+    }
+    const newProduct = await product({ name, category, price, stock });
+  
+    await newProduct.save();
+  
+   return res.json({ message: "product added successfully", product: newProduct });
+  
+  
+}
+  catch (error) {
+   return res.json({ message: "Server error" });
+  }
+});
+
+
+
+
+app.put("/put/:id", async (req, res) => {
+
+    
+    const { name, category, price, stock } = req.body;
+    const id = req.params.id;
+    const update = await product.findByIdAndUpdate(id, { name, category, price, stock })
+    if (update) {
+      res.json({ message: "product updated successfully" });
+    }
+    else {
+      res.json({ message: "product not found" });
+    }
+  });
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const deleted = await product.findByIdAndDelete(id);
+  if (deleted) {
+    res.json({ message: "product deleted successfully" });
+  }
+  else {
+    res.json({ message: "product not exist" });
+  }
+});
+
+
+app.listen(5000, () => {
+  console.log('Server is running.....');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
